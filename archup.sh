6a1a78bf3caeaa5bd6b3ef6ada8dd7def7a23151
mapkts <mapkts@gmail.com>
2021-05-28 16:36:48 +0800

    arch: update archup.sh

--- archup.sh
+++ archup.sh
@@ -45,16 +45,13 @@ boostrap() {
 makeswap() {
 	dd if=/dev/zero of=/swapfile bs=2048 count=1048576 status=progress
 	chmod 600 /swapfile
+	mkswap /swapfile
 	swapon /swapfile
 	echo "/swapfile none swap defaults 0 0" >> /etc/fstab
 }
 
 rootinit() {
-	if [ $# != 1 ]; then
-		echo "Usage: $(basename "$0") rootinit <hostname>" >&2
-		exit 1
-	fi
-	hostname="$1"
+	read -r -p "Choose the hostname you want to use: " $hostname
 
 	ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 	hwclock --systohc
@@ -78,7 +75,7 @@ rootinit() {
 }
 
 wifiinit() {
-	systemctl enable --now NetworkManager
+	sudo systemctl enable --now NetworkManager
 
 	read -r -p "wifi ssid: " ssid
 	read -r -p "wifi password: " passwd
@@ -93,13 +90,13 @@ xorg() {
 	fi
 	driver="$1"
 
-	pacman --noconfirm -S xorg xorg-xinit
+	sudo pacman --noconfirm -S xorg xorg-xinit
 	case "$driver" in
 		intel)
-			pacman --noconfirm -S xf86-video-intel
+			sudo pacman --noconfirm -S xf86-video-intel
 			;;
 		amd)
-			pacman --noconfirm -S xf86-video-amdgpu
+			sudo pacman --noconfirm -S xf86-video-amdgpu
 			;;
 		*)
 			echo "unrecognized graphics driver" >&2
@@ -109,17 +106,16 @@ xorg() {
 }
 
 pkgs() {
-	pacman --noconfirm -S \
-		zsh \
-		sudo \
-		alsa-utils \
+	sudo pacman --noconfirm -S \
+		grub efibootmgr \
+		networkmanager network-manager-applet \
+		dialog wpa_supplicant \
+		os-prober mtools dosfstools \
+		zsh sudo \
+		alsa-utils pulseaudio pulseaudio-alsa pavucontrol \
 		aspell aspell-en hunspell hunspell-en_US \
-		python python-pip python-setuptools python-virtualenv bpython \
-		python2 python2-pip python2-setuptools python2-virtualenv bpython2 \
-		flake8 \
 		go go-tools \
 		rustup \
-		grub efibootmgr \
 		iputils inetutils whois bind-tools dhcpcd \
 		cups \
 		shellcheck \
@@ -127,16 +123,15 @@ pkgs() {
 		cronie openssh openssl sshfs sshuttle gnupg tmux nfs-utils enscript \
 		docker espeak ffmpeg jq ripgrep youtube-dl fzf \
 		iotop htop linux-tools pkgfile pwgen pv strace tig time tree valgrind \
-		git s3cmd colordiff curl wget ctags darkhttpd pass rsync
-	pkgfile --update
+		git s3cmd colordiff curl wget ctags darkhttpd pass rsync \
+		neovim wireshark-cli wireshark-qt
+	sudo pkgfile --update
 }
 
 xpkgs() {
-	pacman --noconfirm -S \
+	sudo pacman --noconfirm -S \
 		xterm \
-		alacritty alacritty-terminfo \
-		k3b konversation konsole dolphin okular \
-		qt5 qt5ct oxygen oxygen-icons \
+		alacritty \
 		xsane \
 		xbindkeys \
 		libnotify \
@@ -166,17 +161,32 @@ yayinit() {
 
 yaypkgs() {
 	yay --noconfirm -S \
-		brother-dcp7065dn \
-		brscan4 \
 		ngrok \
 		par \
-		vim-fzf-git vim-go-git vim-ledger-git vim-plug-git \
-		vim-renamer-git vim-syntastic-git vim-toml-git \
+		vim-plug-git \
 		zsh-autosuggestions \
 		zsh-completions \
 		zsh-syntax-highlighting \
-		zsh-fast-syntax-highlighting-git \
-		dict-wn
+		zsh-fast-syntax-highlighting-git
+}
+
+yayxpkgs() {
+	yay --noconfirm -S \
+		dzen2-xft-xpm-xinerama-git \
+		google-chrome \
+		mirage \
+		idesk \
+		gkrellm \
+		gkleds \
+		gkrellsun \
+		gkrellweather \
+		nerd-fonts-hack \
+		ttf-oxygen \
+		ttf-dejavu \
+		ttf-fira-mono \
+		ttf-mononoki-git \
+		ttf-roboto \
+		ttf-ubuntu-font-family
 }
 
 userinit() {
@@ -195,22 +205,108 @@ grubinit() {
 	grub-mkconfig -o /boot/grub/grub.cfg
 }
 
-case "$1" in
-  netup) shift ; netup "$@" ;;
-  bootstrap) shift ; bootstrap "$@" ;;
-  rootinit) shift ; rootinit "$@" ;;
-  wifiinit) shift ; wifiinit "$@" ;;
-  xorg) shift ; xorg "$@" ;;
-  pkgs) shift ; pkgs "$@" ;;
-  xpkgs) shift ; xpkgs "$@" ;;
-  userinit) shift ; userinit "$@" ;;
-  grubinit) shift ; grubinit "$@" ;;
-  yayinit) shift ; yayinit "$@" ;;
-  yaypkgs) shift ; yaypkgs "$@" ;;
-  yayxpkgs) shift ; yayxpkgs "$@" ;;
+userconf() {
+	# adjust system time
+	# 
+	# https://wiki.archlinux.org/title/System_time
+	sudo timedatectl set-timezone "Asia/Shanghai"
+	sudo timedatectl set-local-rtc true
+	sudo timedatectl set-ntp true
+
+	# diable bell
+	# 
+	# https://wiki.archlinux.org/title/PC_speaker
+	echo "" | sudo tee -a /etc/inputrc 1>/dev/null
+	echo "# do not bell on tab-completion" | sudo tee -a /etc/inputrc 1>/dev/null
+	echo "set bell-style none" | sudo tee -a /etc/inputrc 1>/dev/null
+}
+
+clashproxy() {
+	cd "$HOME" || exit 1
+	mkdir -p bin/clash && cd bin/clash
+
+	read -r -p "input your username: (default: mapkts)? " $uname
+
+	if [[ -z "$uname" ]]; then
+		uname="mapkts"
+	fi
+
+	read -r -p "download which version (default: v1.6.0)? " $clashv
+
+	if [[ -z "$clashv" ]]; then
+		clashv="v1.6.0"
+	fi
+
+	https_proxy="" wget -O clash.gz "https://github.com/Dreamacro/clash/releases/download/$clashv/clash-linux-amd64-$clashv.gz"
+	gunzip "clash.gz"
+	sudo chmod +x clash
+	./clash -v
+
+	clashservice=/lib/systemd/system/clash@.service
+
+	sudo rm -f $clashservice && sudo touch $clashservice
+
+	read -r -p "url to get config.yml: " clashc
+
+	if [[ -z "$clashc" ]]; then
+		echo "url cannot be empty!"
+		exit 1
+	fi
+
+	http_proxy="" https_proxy="" wget -O config.yml "$clashc"
+
+	
+	{
+		echo '[Unit]'
+		echo 'Description=A rule based proxy in Go for %i.'
+		echo 'After=network.target'
+		echo ''
+		echo '[Service]'
+		echo 'Type=simple'
+		echo 'User=%i'
+		echo 'Restart=on-abort'
+		echo "ExecStart=$HOME/bin/clash/clash -f $HOME/bin/clash/config.yml"
+		echo ''
+		echo '[Install]'
+		echo 'WantedBy=multi-user.target'
+	} | sudo tee -a /lib/systemd/system/clash@.service 1>/dev/null
+	
+	sudo systemctl daemon-reload
+	sudo systemctl enable --now "clash@$uname"
+	systemctl status "clash@$uname"
+}
+
+echo ""
+echo "#######################################"
+echo "            Archup Commands            "
+echo "#######################################"
+echo ""
+echo "1) netup         2) bootstrap"
+echo "3) rootinit      4) wifiinit"
+echo "5) xorg          6) pkgs           7) xpkgs"
+echo "8) userinit      9) grubinit"
+echo "10) yayinit      11) yaypkgs       12) yayxpkgs"
+echo "13) userconf     14) clashproxy"
+echo ""
+read -r -p "command to run: " cmd
+
+case $cmd in
+  1) netup ;;
+  2) bootstrap ;;
+  3) rootinit ;;
+  4) wifiinit ;;
+  5) xorg ;;
+  6) pkgs ;;
+  7) xpkgs ;;
+  8) userinit ;;
+  9) grubinit ;;
+  10) yayinit ;;
+  11) yaypkgs ;;
+  12) yayxpkgs ;;
+  13) yayxpkgs ;;
+  14) clashproxy ;;
   *)
     echo "unrecognized command" >&2
-    echo "Usage: ./archup.sh [netup | bootstrap | rootinit arch | wifiinit | xorg | pkgs | xpkgs | userinit | grubinit | yayinit | yaypkgs | yayxpkgs ]"
     exit 1
     ;;
 esac
